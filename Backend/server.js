5827e4771ec39f2a3458785bc5905cbab3c6fa0c
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2026-02-16 15:19:01 +0530

    video routes done almost backend is completed husshhhhh

--- Backend/server.js
+++ Backend/server.js
@@ -4,12 +4,11 @@ const cors = require('cors')
 
 
 const courseRouter = require('./routes/courses')
-// const videosRouter = require('./routes/videos')
-// const studentDataRouter  = require('./routes/studentData')
+const videosRouter = require('./routes/videos')
+const studentDataRouter  = require('./routes/studentData')
 const commonRouter = require('./routes/common')
 const {authUser, checkAuthorization} = require('./utils/auth')
-// const studentRouter = require('./routes/student')
-
+const studentRouter = require('./routes/student')
 
 app.use(cors({
      origin: 'http://localhost:5173', // vite frontend
@@ -21,9 +20,9 @@ app.use(express.json())
 app.use("/", commonRouter)
 app.use(authUser)
 app.use('/courses', courseRouter)
-// app.use('/videos', videosRouter)
-// app.use('/admin', studentDataRouter)
-// app.use('/student', studentRouter)
+app.use('/videos', videosRouter)
+app.use('/admin', studentDataRouter)
+app.use('/student', studentRouter)
 
 
 app.listen(4000, 'localhost', (req, res) => {
