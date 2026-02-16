36f8b42828b0df49bea5a3ce2b4aab9874dc9f88
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2026-02-16 15:03:09 +0530

    my-coursewith-videos for student completed

--- Backend/routes/student.js
+++ Backend/routes/student.js
@@ -38,3 +38,21 @@ router.get('/my-courses', (req, res) => {
         res.send(result.createResult(error, data))
     })
 })
+
+
+///my-coursewith-videos
+router.get('/my-course-with-videos/:course_id', (req, res) => {
+    const email = req.headers.email
+    const Course_id = req.params.course_id
+    const sql = `SELECT c.*, v.video_id, v.title, v.youtube_url, v.added_at,c.video_expire_days 
+                 FROM students s 
+                 INNER JOIN courses c ON s.course_id = c.course_id 
+                 INNER JOIN videos v ON v.course_id = c.course_id 
+                 WHERE s.email = ? AND DATEDIFF(CURDATE(), v.added_at) <= c.video_expire_days AND c.course_id = ?;`
+    pool.query(sql, [email, Course_id], (error, data) => {
+        res.send(result.createResult(error, data))
+    })
+})
+
+
+ module.exports = router;
