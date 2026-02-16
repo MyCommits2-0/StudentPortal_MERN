4f9b2be49591ea6cb153523371c2c3d26942cea1
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2026-02-16 11:47:36 +0530

    get method insode courseroute completed

--- Backend/routes/courses.js
+++ Backend/routes/courses.js
@@ -1,2 +1,13 @@
 const express = require('express')  
 const pool = require('../db/pool')
+const result = require('../utils/result')
+const {checkAuthorization} = require('../utils/auth')
+
+const router = express.Router()
+
+router.get('/all-courses', checkAuthorization, (req, res)=>{
+    const sql = 'SELECT * FROM COURSES'
+    pool.query(sql,(error, data)=>{
+        res.send(result.createResult(error, data))
+    })
+})
