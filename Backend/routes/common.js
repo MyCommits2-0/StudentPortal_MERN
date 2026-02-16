5bc07496b36bb7ce2a4bbcab19312a0c32d926d2
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2026-02-16 14:23:03 +0530

    does user exists validation done inside common route page

---
+++ Backend/routes/common.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const pool = require('../db/pool')
+const result = require('../utils/result')
+const config = require('../utils/config')
+const cryptojs = require('crypto-js')
+const jwt = require('jsonwebtoken')
+
+const router = express.Router()
+
+router.post("/student/register-to-course", (req, res) => {
+    const { name, email, course_id, mobile_no } = req.body
+
+    const sql = "SELECT * FROM users WHERE email=?"
+    pool.query(sql, [email], (error, data) => {
+        if (error) {
+            return res.send(result.createResult(error))
+        }
+        else if (data.length == 0) {
+            const password = "sunbeam";
+
+            const role = "student";
+
+            const usersql = `INSERT INTO users(email,password,role) VALUES(?,?,?)`
+            pool.query(usersql, [email, password, role], (error, data) => {
+
+                if (error) {
+                    return res.send(result.createResult(error))
+                }
+
+                const sql2 = `INSERT INTO students(name, email, course_id, mobile_no) VALUES(?,?,?,?)`
+                pool.query(sql2, [name, email, course_id, mobile_no], (error, data) => {
+                    if (error) {
+                        return res.send(result.createResult(error))
+                    }
+                    res.send(result.createResult(null, data))
+
+                })
+            })
+        }
+        else {
+            const sql=`INSERT INTO students(name,email, course_id,mobile_no) VALUES(?,?,?,?)`;
+             pool.query(sql,[name,email, course_id,mobile_no],(error,data)=>{
+             if(error){
+                 return res.send(result.createResult(error));
+             }
+            res.send(result.createResult(null,data));
+            })
+        }
+    })
+})
--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,11 @@ const cors = require('cors')
 
 
 const courseRouter = require('./routes/courses')
-const videosRouter = require('./routes/videos')
-const studentDataRouter  = require('./routes/studentData')
-const commonRouter = require('./routes/common')
+// const videosRouter = require('./routes/videos')
+// const studentDataRouter  = require('./routes/studentData')
+// const commonRouter = require('./routes/common')
 const {authUser, checkAuthorization} = require('./utils/auth')
-const studentRouter = require('./routes/student')
+// const studentRouter = require('./routes/student')
 
 
 app.use(cors({
@@ -21,9 +21,9 @@ app.use(express.json())
 // app.use("/", commonRouter)
 app.use(authUser)
 app.use('/courses', courseRouter)
-app.use('/videos', videosRouter)
-app.use('/admin', studentDataRouter)
-app.use('/student', studentRouter)
+// app.use('/videos', videosRouter)
+// app.use('/admin', studentDataRouter)
+// app.use('/student', studentRouter)
 
 
 app.listen(4000, 'localhost', (req, res) => {
